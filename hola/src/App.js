f66f952f278526e999c77e2751633b3b13f388fa
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2023-06-28 20:40:46 -0400

    navBar working

--- hola/src/App.js
+++ hola/src/App.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
+import Navbar from "./components/navbar";
 
-function Navbar() {
-  const { t, i18n } = useTranslation();
-
-  function handleClick() {
-    const lang = i18n.language === 'en' ? 'fr' : 'en';
-    i18next.changeLanguage(lang);
-  }
-
-  return (
-    <nav>
-      <div>
-        <a href="/about">{t('About Us')}</a>
-        <a href="/donations">{t('Donations')}</a>
-        <a href="/bachelors">{t('Bachelors')}</a>
-        <a href="/faq">{t('FAQ')}</a>
-        <a href="/contact">{t('Contact Us')}</a>
-      </div>
-      <div>
-        <button onClick={handleClick}>
-          {i18n.language === 'en' ? 'FR' : 'EN'}
-        </button>
-      </div>
-    </nav>
-  );
-}
 
 function App() {
   const { t, i18n } = useTranslation();
-
-  function handleClick() {
-    const lang = i18n.language === 'en' ? 'fr' : 'en';
-    i18next.changeLanguage(lang);
-  }
 
   return (
     <div className="App">
